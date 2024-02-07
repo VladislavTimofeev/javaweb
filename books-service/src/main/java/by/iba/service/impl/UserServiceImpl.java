74827134cd454be29f19fbaefeafd1faa0c4d0d4
Vladislav Timofeev <timofeev.vladislav.sergeevich@gmail.com>
2024-02-07 13:40:19 +0300

    Fix password encryption

--- books-service/src/main/java/by/iba/service/impl/UserServiceImpl.java
+++ books-service/src/main/java/by/iba/service/impl/UserServiceImpl.java
@@ -5,13 +5,11 @@ import by.iba.dto.UserDto;
 import by.iba.exception.ServiceException;
 import by.iba.repository.UserRepository;
 import by.iba.service.UserService;
+import by.iba.util.PasswordHasher;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,12 +18,13 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordHasher passwordHasher;
 
     @Transactional
     @Override
     public UserEntity loginUser(UserDto userDto) {
         Optional<UserEntity> optionalUser = userRepository.findByEmail(userDto.getEmail());
-        if (optionalUser.isPresent() && checkPassword(userDto.getPassword(), optionalUser.get().getPassword())) {
+        if (optionalUser.isPresent() && passwordHasher.isPasswordMatched(userDto.getPassword(), optionalUser.get().getPassword())) {
             return optionalUser.get();
         } else {
             throw new ServiceException("Invalid username or password");
@@ -41,37 +40,9 @@ public class UserServiceImpl implements UserService {
 
         UserEntity newUser = new UserEntity();
         newUser.setEmail(userDto.getEmail());
-        newUser.setPassword(hashPassword(userDto.getPassword()));
+        newUser.setPassword(passwordHasher.getHashedPassword(userDto.getPassword()));
 
         return userRepository.save(newUser);
-    }
-
-    private String hashPassword(String password) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] encodedHash = digest.digest(
-                    password.getBytes(StandardCharsets.UTF_8)
-            );
-            return bytesToHex(encodedHash);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Error hashing password", e);
-        }
-    }
-
-    private boolean checkPassword(String inputPassword, String storedPassword) {
-        return hashPassword(inputPassword).equals(storedPassword);
-    }
-
-    private String bytesToHex(byte[] hash) {
-        StringBuilder hexString = new StringBuilder(2 * hash.length);
-        for (byte b : hash) {
-            String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1) {
-                hexString.append('0');
-            }
-            hexString.append(hex);
-        }
-        return hexString.toString();
     }
 
     @Override
